c7cd5f2f399010106d9940831c347cebe388f411
tomasjon1 <72403454+tomasjon1@users.noreply.github.com>
2022-02-08 00:40:40 +0200

    pseudo code

--- Pazymiai.cpp
+++ Pazymiai.cpp
@@ -1,5 +1,3 @@
-// Pazymiai.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
 
@@ -8,13 +6,15 @@ int main()
     std::cout << "Hello World!\n";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+// 1. 
+
+// susikurti struktura mokiniui
+// funkcija sukasi tiek kiek yra mokiniu
+// funckija nuskaityti duomenis
+//      funkcija nuskaito varda pavarde
+//      funkcija nuskaito mokinio nd skaiciu
+//      funkcija nuskaito mokinio n pazymius
+//      funkcija nuskaito mokinio egzamino rezultata
+//      funkcija apskaiciuoja metini rezultata
+// funkcija atspausdinimui
