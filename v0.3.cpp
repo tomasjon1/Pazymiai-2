4a8ab7197279ceee83973177de0fc6faa438a857
tomasjon1 <72403454+tomasjon1@users.noreply.github.com>
2022-02-27 21:20:14 +0200

    Added header file for data, imports, and using

--- v0.3.cpp
+++ v0.3.cpp
@@ -1,29 +1,5 @@
-﻿#include <iostream>
-#include <fstream>
-#include <iomanip>
-#include <random>
-#include <vector>
-#include <algorithm>
-#include <functional>
+﻿#include "data.h"
 
-using std::cout;
-using std::cin;
-using std::endl;
-using std::string;
-using std::vector;
-using std::ifstream;
-using std::ofstream;
-
-
-struct studentas {
-    string vardas = "";
-    string pavarde = "";
-    vector<int> pazymiai;
-    int pazymiuKiekis = 0;
-    int egzaminas;
-    double rezultatasVid = 0;
-    double rezultatasMed = 0;
-};
 
 void ivedimas(studentas& data, bool generavimas);
 void isvedimas(studentas& data, ofstream& fout);
