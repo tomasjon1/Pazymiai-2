f0d2fccbaa74f798f28348627b443f15d2a9105f
tomasjon1 <72403454+tomasjon1@users.noreply.github.com>
2022-04-21 23:28:17 +0300

    Add files via upload

--- cpp/InputAndOutput.cpp
+++ cpp/InputAndOutput.cpp
@@ -1,25 +1,35 @@
 #include "../headers/InputAndOutput.h"
 
-void ivedimas(studentas& data, bool generavimas) {
+void ivedimas(studentas &data, bool generavimas)
+{
     cout << endl;
-    cout << "Iveskite studento pavarde: "; cin >> data.pavarde;
-    cout << "Iveskite studento varda: "; cin >> data.vardas;
+    cout << "Iveskite studento pavarde: ";
+    cin >> data.pavarde;
+    cout << "Iveskite studento varda: ";
+    cin >> data.vardas;
 
-    cout << "Iveskite studento namu darbu kieki: "; data.pazymiuKiekis = ivestoSkaiciausPatikrinimas();
+    cout << "Iveskite studento namu darbu kieki: ";
+    data.pazymiuKiekis = ivestoSkaiciausPatikrinimas();
     cout << endl;
-    if (generavimas) {
-        for (int x = 0; x < data.pazymiuKiekis; x++) {
+    if (generavimas)
+    {
+        for (int x = 0; x < data.pazymiuKiekis; x++)
+        {
             data.pazymiai.push_back(rand() % 10 + 1);
             cout << "Ivestas " << x + 1 << " pazymyss bus: " << data.pazymiai[x] << endl;
         }
         data.egzaminas = rand() % 10 + 1;
         cout << "Ivestas studento egzamino pazymys bus: " << data.egzaminas << endl;
     }
-    else {
-        for (int x = 0; x < data.pazymiuKiekis; x++) {
-            cout << "Iveskite " << x + 1 << " -a(-i) pazymi: "; data.pazymiai.push_back(ivestiPazymi());
+    else
+    {
+        for (int x = 0; x < data.pazymiuKiekis; x++)
+        {
+            cout << "Iveskite " << x + 1 << " -a(-i) pazymi: ";
+            data.pazymiai.push_back(ivestiPazymi());
         }
-        cout << "iveskite studento egzamino pazymi: "; data.egzaminas = ivestiPazymi();
+        cout << "iveskite studento egzamino pazymi: ";
+        data.egzaminas = ivestiPazymi();
     }
     cout << endl;
 }
@@ -29,23 +39,25 @@ int ivestiPazymi()
     while (true)
     {
         int paz = ivestoSkaiciausPatikrinimas();
-        if (pazymioPatikrinimas(paz)) return paz;
+        if (pazymioPatikrinimas(paz))
+            return paz;
     }
 }
 
-void bufer_read(list<studentas>& studentai, string file_name) {
+void bufer_read(list<studentas> &studentai, string file_name)
+{
 
     std::string line;
     std::stringstream buffer;
 
     std::ifstream open_f(file_name);
 
-
     buffer << open_f.rdbuf();
     open_f.close();
     std::getline(buffer, line);
 
-    while (buffer) {
+    while (buffer)
+    {
         std::getline(buffer, line);
         if (line.length() == 0)
             break;
@@ -58,13 +70,14 @@ void bufer_read(list<studentas>& studentai, string file_name) {
 
             t.pazymiai.push_back(mark);
         }
-            t.egzaminas = t.pazymiai.back();
-            t.pazymiai.pop_back();
-            studentai.push_back(t);
+        t.egzaminas = t.pazymiai.back();
+        t.pazymiai.pop_back();
+        studentai.push_back(t);
     }
 }
 
-void bufer_write(list<studentas>& studentai) {
+void bufer_write(list<studentas> &studentai)
+{
 
     std::stringstream outputas;
     outputas << std::left << std::setw(20) << "Vardas";
@@ -73,7 +86,8 @@ void bufer_write(list<studentas>& studentai) {
     outputas << std::left << std::setw(20) << "Galutinis (Med.)";
     outputas << endl;
 
-    for (auto& stud : studentai) {
+    for (auto &stud : studentai)
+    {
 
         outputas << std::left << std::setw(20) << stud.vardas;
         outputas << std::left << std::setw(20) << stud.pavarde;
@@ -82,13 +96,25 @@ void bufer_write(list<studentas>& studentai) {
         outputas << endl;
     }
 
-    ssToFile("rez.txt", outputas);
-}
-
-void ssToFile(string file_name, std::stringstream& data)
-{
-    std::ofstream out_f(file_name);
-    out_f << data.rdbuf();
+    studentai.clear();
+    std::ofstream out_f("rez.txt");
+    out_f << outputas.rdbuf();
     out_f.close();
 }
 
+void listToFile(string file_name, list<studentas> &data)
+{
+    std::stringstream outputas;
+    outputas << std::left << std::setw(20) << "Vardas" << std::left << std::setw(20) << "Pavarde" << std::left << std::setw(20) << "Vid." << std::left << std::setw(20) << "Med." << endl;
+    for (auto &stud : data)
+    {
+        outputas << std::left << std::setw(20) << stud.vardas;
+        outputas << std::left << std::setw(20) << stud.pavarde;
+        outputas << std::left << std::setw(20) << stud.rezultatasVid;
+        outputas << std::left << std::setw(20) << stud.rezultatasMed;
+        outputas << endl;
+    }
+    std::ofstream out_f(file_name);
+    out_f << outputas.rdbuf();
+    out_f.close();
+}
